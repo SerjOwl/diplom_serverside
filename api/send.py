7535a52321d7b3d3b23cfe94b36fec0eafbd2be7
Tura <you@mail.ru>
2025-02-22 01:17:12 +0300

    refactor a bit send

--- api/send.py
+++ api/send.py
@@ -4,12 +4,14 @@ from config import Config
 config = Config()
 
 url = f"http://{config.host}:{config.port}/get-name"
-
 params = {
     "input_content": "Привет",
     "deep_think": False,
     "print_log": True
 }
-
 response = requests.get(url, params=params)
-print(response)
+
+if response.status_code == 200:
+    print(response.text)
+else: 
+    print(f"Error: {response.status_code}")
